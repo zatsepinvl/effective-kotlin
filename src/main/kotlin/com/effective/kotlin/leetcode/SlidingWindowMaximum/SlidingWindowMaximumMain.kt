57b8f3eff3d880d330d2772b6c5eb3068b6cb53b
Vladimir Zatsepin <zatsepin.vl@gmail.com>
2023-03-27 15:37:07 +0200

    Commit changes

--- src/main/kotlin/com/effective/kotlin/leetcode/SlidingWindowMaximum/SlidingWindowMaximumMain.kt
+++ src/main/kotlin/com/effective/kotlin/leetcode/SlidingWindowMaximum/SlidingWindowMaximumMain.kt
@@ -1,12 +1,13 @@
 package com.effective.kotlin.leetcode.SlidingWindowMaximum
 
 import java.awt.SystemColor.window
+import java.util.PriorityQueue
 
 fun main() {
     val solution = Solution()
 
     val result = solution.maxSlidingWindow(
-        intArrayOf(1,3,-1,-3,5,3,6,7),
+        intArrayOf(1, 3, -1, -3, 5, 3, 6, 7),
         3
     )
 
@@ -16,19 +17,14 @@ fun main() {
 class Solution {
     fun maxSlidingWindow(nums: IntArray, k: Int): IntArray {
         val result = IntArray(nums.size - k + 1)
-        var lastMax = nums.take(k).max()!!
+        val queue = PriorityQueue<Int>(k) { a, b -> b - a }
+        nums.take(k).forEach { queue.add(it) }
+        result[0] = queue.peek()
 
-        for (step in result.indices) {
-            var max = nums[step + k - 1]
-            if (max < lastMax) {
-                for (i in step until step + k) {
-                    if (nums[i] > max) {
-                        max = nums[i]
-                    }
-                }
-            }
-            lastMax = max
-            result[step] = max
+        for (step in 1..result.lastIndex) {
+            queue.remove(nums[step - 1])
+            queue.add(nums[step + k -1])
+            result[step] = queue.peek()
         }
 
         return result
