ef5a4252f08c5a2614209a86473fc4029dad4870
Vladimir Zatsepin <zatsepin.vl@gmail.com>
2023-04-09 18:02:40 +0200

    Add TreesPlayground.kt

---
+++ src/main/kotlin/com/effective/kotlin/leetcode/Trees/TreesPlayground.kt
@@ -0,0 +1,35 @@
+package com.effective.kotlin.leetcode
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int = 0) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+
+class TreeNode(var `val`: Int = 0) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+}
+
+class Solution {
+    fun lowestCommonAncestor(root: TreeNode?, p: TreeNode?, q: TreeNode?): TreeNode? {
+        if (root == null) {
+            return null
+        }
+
+        if (root == p || root == q) {
+            return root
+        }
+
+        val left = lowestCommonAncestor(root.left, p, q)
+        val right = lowestCommonAncestor(root.right, p, q)
+
+        if (left != null && right != null) {
+            return root
+        }
+
+        return left ?: right
+    }
+}
